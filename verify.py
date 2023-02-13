43efc2ef09cdd5c4d399e64b5e74936a4ca61cd3
Anay Pareek <anaypareek2003@gmail.com>
2023-02-13 17:00:06 +0530

    Add documentation and restructure code

--- verify.py
+++ verify.py
@@ -2,71 +2,97 @@ import cv2
 import numpy as np
 import os
 from deepface import DeepFace
-import sys
 
+# Take roll no from user
 roll_no = input("Please enter your roll no: ")
+
+# Check if the user's JPEG exists and exit if it doesn't
 if not os.path.isfile(f"data/{roll_no}.jpg"):
     print("This roll no is not registered.")
-    sys.exit()
+else:
+    # Start capturing the webcam, set its width and height, and create a window for displaying its output
+    capture = cv2.VideoCapture(0)
+    frame_width = 1280
+    frame_height = 720
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    cv2.namedWindow("Verify", cv2.WINDOW_AUTOSIZE)
 
-confidence_limit = 0.8
+    # Load SSD model for face detection and set threshold
+    net = cv2.dnn.readNetFromCaffe(
+        os.path.join(os.path.expanduser("~"), ".deepface/weights/deploy.prototxt"),
+        os.path.join(os.path.expanduser("~"), ".deepface/weights/res10_300x300_ssd_iter_140000.caffemodel")
+    )
+    thresh = 0.8
 
-capture = cv2.VideoCapture(0)
-cv2.namedWindow("Face detector", cv2.WINDOW_AUTOSIZE)
+    while True:
+        # Read a frame from the webcam, flip it and make a copy of it
+        _, frame = capture.read()
+        frame = cv2.flip(frame, 1)
+        frame_copy = np.copy(frame)
 
-net = cv2.dnn.readNetFromCaffe(
-    os.path.join(os.path.expanduser("~"), ".deepface/weights/deploy.prototxt"),
-    os.path.join(os.path.expanduser("~"), ".deepface/weights/res10_300x300_ssd_iter_140000.caffemodel")
-)
+        # Initialize a variable to store the no of faces detected in the frame
+        no_of_faces = 0
 
-while True:
-    _, frame = capture.read()
-    frame_flip = cv2.flip(frame, 1)
-    frame_flip_copy = np.copy(frame_flip)
+        # Create a blob from the frame and pass it to the face detector model
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
+        net.setInput(blob)
+        detections = net.forward()
 
-    no_of_faces = 0
+        # Loop through all the detections
+        for i in range(detections.shape[2]):
+            # Retrieve the confidence level and ignore detection if the it is lower than the threshold
+            confidence = detections[0, 0, i, 2]
+            if confidence < thresh:
+                continue
 
-    h, w = frame_flip.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame_flip, 1.0, (300, 300), (104.0, 177.0, 123.0))
-    net.setInput(blob)
-    detections = net.forward()
+            # Increment the no of faces by one
+            no_of_faces += 1
 
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence < confidence_limit:
-            continue
+            # Get the coordinates for the detected face
+            box = detections[0, 0, i, 3:7] * np.array([frame_width, frame_height, frame_width, frame_height])
+            start_x, start_y, end_x, end_y = box.astype("int")
 
-        no_of_faces += 1
+            # Create an outline around the detected face and display the confidence percentage on the frame
+            text = f"{confidence * 100:.2f}%"
+            y = start_y - 10 if start_y - 10 > 10 else start_y + 10
+            cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+            cv2.putText(frame, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
 
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        start_x, start_y, end_x, end_y = box.astype("int")
+        # Get keypress input
+        key = cv2.waitKey(1) & 0xFF
 
-        text = f"{confidence * 100:.2f}%"
-        y = start_y - 10 if start_y - 10 > 10 else start_y + 10
-        cv2.rectangle(frame_flip, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
-        cv2.putText(frame_flip, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
-
-    key = cv2.waitKey(1) & 0xFF
-
-    if key == ord('q'):
-        break
-    elif key == ord(' '):
-        if no_of_faces == 0:
-            print("No face detected!")
-        elif no_of_faces == 1:
-            face = cv2.imread(f"data/{roll_no}.jpg")
-            result = DeepFace.verify(face, frame_flip_copy, "Facenet512", "ssd", "euclidean_l2")
-            print(f"Distance: {result['distance']} (threshold: {result['threshold']})")
-            print(f"Time taken: {result['time']}s")
-            if result["verified"]:
-                print("Verified!")
-            else:
-                print("Not verified!")
+        if key == ord('q'):
+            # Exit loop if 'q' is pressed
+            print("Exiting.")
             break
-        else:
-            print("More than one face detected!")
+        elif key == ord(' '):
+            # If the spacebar key is pressed and only one face is detected, try to verify else continue
+            if no_of_faces == 0:
+                print("No face detected!")
+            elif no_of_faces == 1:
+                # User's JPEG and webcam frame are passed to verify if they contain the same person's face
+                # Model is chosen as a modified FaceNet which returns 512 dimensional vector rather
+                # than the usual 128 dimensional vector
+                # The face detector chosen is SSD, the same as the one used in this file.
+                face = cv2.imread(f"data/{roll_no}.jpg")
+                result = DeepFace.verify(face, frame_copy, "Facenet512", "ssd", "euclidean_l2")
 
-    cv2.imshow("Face detector", frame_flip)
+                # The distance between the two images, time taken by the model and verification status
+                # are all printed
+                print(f"Distance: {result['distance']} (threshold: {result['threshold']})")
+                print(f"Time taken: {result['time']}s")
+                if result["verified"]:
+                    print("Verified!")
+                else:
+                    print("Not verified!")
+                break
+            else:
+                print("More than one face detected!")
 
-capture.release()
-cv2.destroyWindow("Face detector")
+        # Display the frame on the window
+        cv2.imshow("Verify", frame)
+
+    # Turn the webcam off and destroy the window
+    capture.release()
+    cv2.destroyWindow("Verify")
